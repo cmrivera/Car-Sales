0d0dde3e2556f98b2dbb59cabcc7f1bfbff00eaf
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-23 16:22:04 -0400

    changed reducer  and removed extra files.

--- src/Reducers/featuresReducer.js
+++ src/Reducers/featuresReducer.js
@@ -1,10 +1,32 @@
-const addRemoveFeature = (state = 0, action) => {
-  switch (action.type) {
-    case "Add_Feature":
-      return state + 1;
-    case "Remove_Feature":
-      return state - 1;
-  }
+export const inistialState = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: [],
+  },
+  additionalFeatures: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 },
+  ],
 };
 
-export default addRemoveFeature;
+export const reducer = (state = inistialState, action) => {
+  switch (action.type) {
+    case "ADD_FEATURES":
+      console.log("action.payload", action.payload);
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          features: [...state.car.features, action.payload],
+        },
+      };
+    default:
+      return state;
+  }
+};
--- src/Reducers/totalUpdate.js
+++
@@ -1,6 +0,0 @@
-const updateTotalReducer = (state = false , action) => {
-switch(action.type) {
-  case 'Update_Total':
-    return !state;
-}
-}export default updateTotalReducer;
