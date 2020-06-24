96e951b9db83bcdc56c652565f57b785474cd6a3
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-23 20:11:32 -0400

    fxed issues from 1:1

--- src/Actions/actions.js
+++ src/Actions/actions.js
@@ -2,16 +2,16 @@
 // aka a function that returns an action object
 
 // action creator
-export const Add_Features = "Add_Features";
+
 export const addFeatures = (item) => {
   console.log(item);
   // the returned object is an action
   return { type: "ADD_FEATURES", payload: item };
 };
 
-export const Remove_Features = "Remove_Features";
 export const removeFeatures = (item) => {
-  return { type: "Remove_Features", payload: item };
+  console.log("remove");
+  return { type: "REMOVE_FEATURES", payload: item };
 };
 
 //   export const updateTitle = newTitle => {
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -18,7 +18,7 @@ const AddedFeature = (props) => {
       >
         X
       </button>
-      {props.feature.name}
+      {props.feature.name}(-{props.feature.price})
     </li>
   );
 };
